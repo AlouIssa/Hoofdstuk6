813763c250f98ad44a13d6c832218124186deb20
Farid Alou Issa <mohamed.alouissa@vdabcampus.be>
2021-02-01 21:45:36 +0100

    T.e.m. Einde hfdst 6

---
+++ src/main/java/be/vdab/oauth/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package be.vdab.oauth.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.oauth2Login();
+        http.authorizeRequests(request->request
+                .mvcMatchers("/").permitAll()
+                .mvcMatchers("/beveiligd").authenticated());
+    }
+}
